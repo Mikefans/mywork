11fe4e14021090ef7c5bf611ec056fbec60f9df7
qingfeng <917481860@qq.com>
2015-05-25 19:43:24 +0800

    1

--- admin/controllers/Index.php
+++ admin/controllers/Index.php
@@ -2,10 +2,7 @@
 class IndexController extends Controllers_Abstract
 {
    public function indexAction()
-   {
-       $this->disableView();
-       $this->redirect('/home/index/index');
-   }
+   {}
    
    public function loginAction()
    {
